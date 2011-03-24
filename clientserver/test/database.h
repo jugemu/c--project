bb855552887fc7bf9a3e1d6b716509287acac96f
kuba <kuba@matrixa2.(none)>
2011-03-24 14:52:55 +0100

    update

--- clientserver/test/database.h
+++ clientserver/test/database.h
@@ -19,17 +19,36 @@ class Database
   typedef map<int, newsgroup> MapType;
   
 public:
+
+	// Creates a new instance of the Database
   static Database& instance();
+  
+  // Return a string with the listed newsgroups
   string listNewsgroups();
+  
+  // Create a new newsgroup and add it to the database
   int addNewsgroup(string title);
+  
+  // TERMINATE D: LINEPIECE D:
   int delNewsgroup(int newsId);
+  
+  // Lists articles in newsgroup
   string listArticles(int newsId);
+  
+  // Adds article to newsgroup
   int addArticle(int newsId, string title, string author, string text);
+  
+  // Delete article in newsgroup
   int delArticle(int newsId, int artId);
+  
+  // Gets article from newgroup
   string getArticle(int newsId, int artId);
     
 private:
+  // Constructs an empty database
   Database(){};
+  
+  // Declares a Map map<int, pair<string, map<int, article> > >
   MapType newsDb;
 };
 #endif
